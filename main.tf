97890ddd761ea6b93a60b06306c0dba00518991d
Tech Architect <kanjikaris@gmail.com>
2026-07-03 09:46:32 +0200

    Web server configuration

--- main.tf
+++ main.tf
@@ -1,8 +1,155 @@
-resource "aws_instance" "example" {
-    ami = "ami-06067086cf86c58e6"
-    instance_type = "t2.micro"
+# Resource replaced
+# resource "aws_instance" "example" {
+#     ami = "ami-06067086cf86c58e6"
+#     instance_type = "t2.micro"
+#    vpc_security_group_ids = [aws_security_group.instance.id]
 
-    tags = { 
-        Name = "terraform-example"
-    }
+#     user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World!" > index.html
+#               nohup busybox httpd -f -p ${var.server_port} &
+#               EOF
+
+#     user_data_replace_on_change = true
+
+#     tags = { 
+#         Name = "terraform-example"
+#     }
+# }
+
+resource "aws_security_group" "instance" {
+  name        = "terraform-example-instance-sg"
+  description = "Security group for the Terraform example instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+resource "aws_launch_template" "example" {
+  image_id        = "ami-06067086cf86c58e6"
+  instance_type   = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data =base64encode(<<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+)
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example_asg" {
+  launch_template {
+    id = aws_launch_template.example.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+#   tag = [{
+#       key                 = "Name"
+#       value               = "terraform-asg-example"
+#       propagate_at_launch = true
+#     }]
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  #allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #allow outbound HTTP requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+
