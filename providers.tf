a1f0d975d71ef7fadf9f8c48d5f0d48efc314e06
Tech Architect <kanjikaris@gmail.com>
2026-07-08 12:06:20 +0200

    Add S3 backend bootstrap and configure root module remote state

--- providers.tf
+++ providers.tf
@@ -11,3 +11,13 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "kanji-terraform-state-bucket"
+    key            = "root/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile     = true
+    encrypt        = true
+  }
+}
